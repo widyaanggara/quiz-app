81aa1a9729539e5cdbeed7ed40b382b88810b12c
Widya Anggara <anggarawidya005@gmail.com>
2025-03-30 16:02:00 +0800

    menghapus beberapa hal

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
+
+import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
 
 export default function Navbar() {
   return (
     <div className="w-screen fixed top-0 left-0 py-5 text-black bg-white/30 dark:bg-[#0a0a0a]/30 backdrop-blur-sm px-6  border-b">
       <div className="max-w-6xl mx-auto flex items-center justify-between">
-        <a href="/" className='text-black dark:text-white font-bold text-2xl md:text-3xl cursor-pointer'>
+        <Link href="/" className='text-black dark:text-white font-bold text-2xl md:text-3xl cursor-pointer'>
           Quizzy
-        </a>
+        </Link>
         <div>
           <ModeToggle />
         </div>
